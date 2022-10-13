7bc56d6e3a7ba5280f94071b16845c3f7c89e81c
naveen-duggal <84651021+naveen-duggal@users.noreply.github.com>
2022-10-13 13:16:40 +0530

    Update ascii.java
    char : ....

--- ascii.java
+++ ascii.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ascii {
     public static void main(String[] args){
         char ch;
-        System.out.println("Enter any Character");
+        System.out.println("Enter any Character: ");
         Scanner r = new Scanner(System.in);
 
         int a = ch;
