19ee197c52ad3f65fba62c63ee2353d2ca16e668
naveen_duggal <naveenduggal132101@gmail.com>
2022-10-13 12:01:25 +0530

    nav1

---
+++ ascii.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ascii {
+    public static void main(String[] args){
+        char ch;
+        System.out.println("Enter any Character");
+        Scanner r = new Scanner(System.in);
+
+        int a = ch;
+        System.out.println("ASCII value of "+ch +" is " + a);    
+    }
+}
